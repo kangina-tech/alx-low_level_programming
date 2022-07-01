4b6113e29f7776e9f0126bb2a2293a8076fb3379
kangina-tech <debbienyasetia@gmail.com>
2022-07-01 02:18:23 -0700

    Task 0,1,2,3,4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int y;
+for (y = 97; y <= 122; y++)
+{
+<< << << << HEAD
+if (m == 101 || m == 113)
+== == == ==
+if (y == 101 || y == 113)
+>> >> >> >>
+{
+}
+else
+{
+putchar(y);
+}
+}
+putchar('\n');
+return (0);
+}
