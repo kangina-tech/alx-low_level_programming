dbc277181ec808bf486ac5d1f29b4f8e8f60dad7
kangina-tech <debbienyasetia@gmail.com>
2022-07-05 05:08:06 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - gives the absolute value of an integer
+ * @n: input number as integer
+ * return: 0 or 1
+ */
+int _abs(int n)
+{
+return (n >= 0 ? n : n * -1);
+}
