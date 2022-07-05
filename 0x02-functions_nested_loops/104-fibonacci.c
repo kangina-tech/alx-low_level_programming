ba4a389a01605dd3df1cea35382ae7c01457fef0
kangina-tech <debbienyasetia@gmail.com>
2022-07-05 08:54:07 -0700

    submit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,7 +5,6 @@
  * Return: 0
  */
 int main(void)
-
 {
 int counter, overflow;
 unsigned long a = 1;
@@ -27,8 +26,8 @@ b_tail = b % 1000000000;
 for (; counter < 99; counter++)
 {
 overflow = (a_tail + b_tail) / 1000000000;
-sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
-sum_head = (a_head + b_head) + overflow;
+sum_tail = (a_tail + b_tail) -(1000000000 * overflow);
+sum_head = (a_head + b_head) +overflow;
 printf(", %lu%lu", sum_head, sum_tail);
 a_head = b_head;
 a_tail = b_tail;
