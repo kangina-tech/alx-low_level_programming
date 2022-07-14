6ee763f6a23a9ca4ed6c453f11981247fbe99f4f
kangina-tech <debbienyasetia@gmail.com>
2022-07-14 03:26:30 -0700

    submit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 -  encodes a string using rot13
+ * @str:the string targeted
+ * Return: returns the encoded string
+ */
+
+char *rot13(char *str)
+{
+int i = 0, j;
+char s[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (; str[i] != '\0'; i++)
+{
+for (j = 0; j <= 51; j++)
+{
+if (s[j] == str[i])
+{
+str[i] = s1[j];
+}
+}
+}
+return (str);
+}
