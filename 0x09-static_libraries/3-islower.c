ddff7abd75b01811572c96310a3e248fe63106b8
Kangina <85179015+kangina-tech@users.noreply.github.com>
2022-07-22 10:33:17 +0300

    Create 3-islower.c

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: character to check
+ * Return: 1 if lowercase, 0 if uppercase
+ */
+int _islower(int c)
+{
+return (c >= 97 && c <= 122);
+}
