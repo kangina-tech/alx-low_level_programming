8f860824826043ba68c6af27b608e350fb1ae539
Kangina <85179015+kangina-tech@users.noreply.github.com>
2022-07-22 10:33:58 +0300

    Create 6-abs.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - gives the absolute value of an integer
+ * @n: input number as integer
+ * Return: 0 or 1
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (n * -1);
+}
+}
