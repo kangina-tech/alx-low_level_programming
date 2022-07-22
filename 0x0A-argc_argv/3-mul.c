09cd36eee7f74b76ec779fbae7373bf16f4347fb
Kangina <85179015+kangina-tech@users.noreply.github.com>
2022-07-22 11:43:07 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,24 +2,29 @@
 #include <stdlib.h>
 
 /**
- * main - prints multiplication
- * of two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: returns 0
- */
+ * main - prints the function.
+ * @argc: counts agruments passed to the function for int.
+ * @argv: gives the value passed to the function for char.
+ *
+ * Return: Returns always success.
+**/
 
 int main(int argc, char *argv[])
 {
-int i, j, mul;
-if (argc <= 2)
+int i, res = 1;
+
+if (argc != 3)
 {
-printf("Error\n");
+printf("%s\n", "Error");
 return (1);
 }
-i = atoi(argv[1]);
-j = atoi(argv[2]);
-mul = i * j;
-printf("%d\n", mul);
+else
+{
+for (i = 1; i < argc; i++)
+{
+res *= atoi(argv[i]);
+}
+printf("%d\n", res);
+}
 return (0);
 }
