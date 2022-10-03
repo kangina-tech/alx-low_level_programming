cdb6842c6e33ff33655c4ed69ab922d357e72cab
kangina_tech <debbienyasetia@gmail.com>
2022-10-03 04:21:04 -0700

    submit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/kangina-tech/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P ../ https://github.com/kangina-tech/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD=../libhack.so
